64638029824b02b5ae9ee01b169353dd765b3390
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-11-10 23:05:53 -0500

    Initial Commit

---
+++ HW 4/HW_4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_4App.swift
+//  HW 4
+//
+//  Created by Cruz Gonzalez Garcia on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HW_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
